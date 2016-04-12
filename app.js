9fe3518529626ffe4f18eb55f6750f0c0e9f6ed6
pszczolkowski <pszczolkowski.k@gmail.com>
2016-04-12 11:20:10 +0200

    Run new instance

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var lab1_1 = require("./lab/lab1_1").lab;
 var example_1 = require("./example_1").lab;
-var lab2 = require("./lab/lab2").info;
+var lab2 = require("./lab/lab2");
 
 var PORT = 8080;
 
@@ -8,7 +8,8 @@ var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
-	{path: "/lab2", action: lab2}
+	{path: "/lab2", action: lab2.info},
+	{path: "/runInstance", action: lab2.runInstance}
 	];
 
 var service = require("./lib/service").http(urlMap);
--- lab/lab2.js
+++ lab/lab2.js
@@ -1,14 +1,15 @@
 var AWS = require('aws-sdk');
 
 module.exports = {
-	info: info
+	info: info,
+	runInstance: runInstance
 };
 
 
 AWS.config.loadFromPath('./config.json');
+var ec2 = new AWS.EC2();
 
 function info(request, callback) {
-	var ec2 = new AWS.EC2();
 	var params = {
 		DryRun: false,
 		MaxResults: 10
@@ -20,6 +21,38 @@ function info(request, callback) {
 		}
 		else {
 			callback(null, data);
+		}
+	});
+}
+
+function runInstance(request, callback) {
+	var params = {
+		ImageId: 'ami-80f715e0',
+		MaxCount: 1,
+		MinCount: 1,
+		Monitoring: {
+			Enabled: false 
+		},
+		InstanceType: 't1.micro'
+	};
+	ec2.runInstances(params, function(err, data) {
+		if (err) {
+			callback(err);
+		} else {
+			var params = {
+				DryRun: false,
+				InstanceIds: [data.Instances[0].InstanceId]
+			};
+			
+			ec2.describeInstances(params, function(err, data) {
+				if (err) {
+					callback(err); 
+				}
+				else {
+					callback(null, "DNS: " + data.Reservations[0].Instances[0].PublicDnsName + "<br />" + "IP: " + data.Reservations[0].Instances[0].PublicIpAddress);
+				}
+			});
+		
 		}
 	});
 }
