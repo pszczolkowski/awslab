e35e89fff153748b60fe81b1faf5810150adf08d
pszczolkowski <pszczolkowski.k@gmail.com>
2016-04-12 10:02:07 +0200

    Add simple task

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
+var lab2 = require("./lab/lab2").lab;
+var AWS = require('awssdk');
 
 var PORT = 8080;
 
+AWS.config.loadFromPath('./config.json');
 
 var urlMap = [
 	{path: "/", action:__dirname + "/static/index.html"},	 
 	{path: "/digest", action: lab1_1},	
 	{path: "/example_1", action: example_1}, 
+	{path: "/lab/lab2", action: lab2}
 	];
 
 var service = require("./lib/service").http(urlMap);
